00246e7f750df393f3c2bf4db2d8f2885bc765ae
burakhammusoglu <mburak.hammusoglu@gmail.com>
2020-03-15 18:01:24 +0300

    Platformgamev1

---
+++ Assets/characterControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float hiz;
+    void Start()
+
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontal * hiz*Time.deltaTime,0);
+    }
+}
